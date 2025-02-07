214fb28c96f324e42fc882c38111c5a1e5498644
TJ Strickland <thomaskeith17@gmail.com>
2025-02-07 16:32:46 -0700

    swagger implemented

--- routes/contacts.js
+++ routes/contacts.js
@@ -37,10 +37,117 @@ router.get('/', contactsController.getAll);
  */
 router.get('/:id', contactsController.getSingle);
 
+/**
+ * @swagger
+ * /contacts:
+ *   post:
+ *     summary: Create a new user
+ *     description: Add a new user to the database.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - firstName
+ *               - lastName
+ *               - email
+ *               - favoriteColor
+ *               - birthday
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *                 example: "Lorraine"
+ *               lastName:
+ *                 type: string
+ *                 example: "Strickland"
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: "lorraine@email.com"
+ *               favoriteColor:
+ *                 type: string
+ *                 example: "blue"
+ *               birthday:
+ *                 type: string
+ *                 format: date
+ *                 example: "1990-08-01"
+ *     responses:
+ *       201:
+ *         description: User created successfully.
+ *       400:
+ *         description: Invalid input data.
+ */
+
 router.post('/', contactsController.createContact);
 
+
+/**
+ * @swagger
+ * /contacts/{_id}:
+ *   put:
+ *     summary: Update a user's information
+ *     description: Update an existing user's details in the database.
+ *     parameters:
+ *       - in: path
+ *         name: _id
+ *         required: true
+ *         description: The MongoDB ObjectId of the user to update.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *                 example: "Lorraine"
+ *               lastName:
+ *                 type: string
+ *                 example: "Strickland"
+ *               email:
+ *                 type: string
+ *                 example: "lorrines@email.com"
+ *               favoriteColor:
+ *                 type: string
+ *                 example: "blue"
+ *               birthday:
+ *                 type: string
+ *                 format: date
+ *                 example: "1990-08-01"
+ *     responses:
+ *       200:
+ *         description: User updated successfully.
+ *       400:
+ *         description: Invalid input data.
+ *       404:
+ *         description: User not found.
+ */
 router.put('/:id', contactsController.updateContact);
 
+/**
+ * @swagger
+ * /contacts/{_id}:
+ *   delete:
+ *     summary: Delete a user
+ *     description: Remove a user from the database using their unique _id.
+ *     parameters:
+ *       - in: path
+ *         name: _id
+ *         required: true
+ *         description: The MongoDB ObjectId of the user to delete.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: User deleted successfully.
+ *       404:
+ *         description: User not found.
+ */
 router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
