a39c59b764751dc3ac90a2e6e127df5dafa7602e
TeaJay13 <131283247+TeaJay13@users.noreply.github.com>
2025-02-07 00:23:23 -0700

    swagger part 1

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,38 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+/**
+ * @swagger
+ * /contacts:
+ *   get:
+ *     summary: Get a list of users
+ *     description: Retrieve a list of users from the database.
+ *     responses:
+ *       200:
+ *         description: Successful response with a list of users.
+ */
 router.get('/', contactsController.getAll);
 
+
+/**
+ * @swagger
+ * /contacts/{_id}:
+ *   get:
+ *     summary: Get a single user
+ *     description: Retrieve a single user from the database using their unique _id.
+ *     parameters:
+ *       - in: path
+ *         name: _id
+ *         required: true
+ *         description: The MongoDB ObjectId of the user to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful request, user found.
+ *       404:
+ *         description: User not found.
+ */
 router.get('/:id', contactsController.getSingle);
 
 router.post('/', contactsController.createContact);
