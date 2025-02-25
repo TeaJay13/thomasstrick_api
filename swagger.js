1d77eab1e34da95c0bb7777088a59e61f6b1d263
TeaJay13 <131283247+TeaJay13@users.noreply.github.com>
2025-02-25 15:02:57 -0700

    Revert "camp api creation"
    This reverts commit 933360254fa87b62946fdad4c7c383577c019e6d.

--- swagger.js
+++ swagger.js
@@ -4,9 +4,9 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'campsites-api',
+      title: 'tjstrickland-api',
       version: '1.0.0',
-      description: 'API documentation for toop 10 campsites in Idaho',
+      description: 'API documentation for your Node.js application',
     },
   },
   apis: ['./routes/*.js'], // Path to the API routes
